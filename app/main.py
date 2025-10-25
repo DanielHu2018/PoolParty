b6403749f1c8888462a25815054878df19dc46a9
Arnav5321 <malisettyarnav@gmail.com>
2025-10-25 14:36:08 -0400

    functions for view listing and ride management with the .html files

--- app/main.py
+++ app/main.py
@@ -6,3 +6,17 @@ views and route handlers here when implementing the application logic.
 """
 
 __all__ = []
+
+@main_bp.route('/listings')
+def listings():
+    pools = Pool.query.order_by(Pool.depart_time.asc().nullsfirst()).all()
+    return render_template('listings.html', pools=pools)
+
+
+@main_bp.route('/manage')
+@login_required
+def manage():
+    # show pools owned by user and join requests for those pools
+    owned_pools = Pool.query.filter_by(owner_id=current_user.id).all()
+    my_requests = JoinRequest.query.filter_by(user_id=current_user.id).all()
+    return render_template('manage.html', owned_pools=owned_pools, my_requests=my_requests)
